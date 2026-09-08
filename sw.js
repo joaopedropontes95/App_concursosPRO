a901c169ef75dd123af22effafdba493d9617323
joaopedropontes95 <joaopedroafpontes@gmail.com>
2026-09-08 11:09:44 -0300

    feat: adicionar service worker PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='concursospro-v1';const ASSETS=['./','./index.html','./app.html','./manifest.webmanifest','./sw.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
