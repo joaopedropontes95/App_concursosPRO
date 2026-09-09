0b27e0abf2a59bdf5ec7051dde89ab85a9ebf300
joaopedropontes95 <joaopedroafpontes@gmail.com>
2026-09-09 12:02:00 -0300

    QA: separate baseline from versioned regressions

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  testMatch: 'app.spec.js',
   timeout: 45000,
   expect: { timeout: 10000 },
   fullyParallel: false,
